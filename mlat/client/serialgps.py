177246a73b6ce7e6bb3b95ec114297eb93d630f4
ybb <yangbinbin_ytu@163.com>
2017-06-02 18:35:22 -0700

    modify gps

--- mlat/client/serialgps.py
+++ mlat/client/serialgps.py
@@ -1,11 +1,11 @@
-import serial
 import pynmea2
-import time
-import _thread
+import serial
+
 from mlat.client.util import monotonic_time
 
+
 class MSerialPort:
-    report_interval = 2s0.0
+    report_interval = 20.0
 
     def __init__(self,port,buand,coor):
         self.port=serial.Serial(port,baudrate=buand )
@@ -21,10 +21,10 @@ class MSerialPort:
 
     def read_data(self):
         while True:
-            data=self.port.readline()
-            line = data.decode()
-            if (line.startswith("$GPGGA")):
-                try:
+            try:
+                data=self.port.readline()
+                line = data.decode()
+                if (line.startswith("$GPGGA")):
                     rmc = pynmea2.parse(line, check=False)
                     lon = rmc.longitude
                     lat = rmc.latitude
@@ -32,12 +32,11 @@ class MSerialPort:
                     if(lon==0 or lat==0):
                         continue
                     if(self.coordinator):
-                        #print(line , lon ,lat , alt)
                         now =monotonic_time()
                         if now > self.next_update_time:
                             self.coordinator.gps_position_update_event(lat,lon,alt)
                             self.next_update_time = monotonic_time() + self.report_interval
-                except Exception:
-                    continue
+            except Exception:
+                continue
 
 
