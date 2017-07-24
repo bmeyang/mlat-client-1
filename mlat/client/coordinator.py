e817d78fa7d4401449781237766f755acc46e9db
ybb <yangbinbin_ytu@163.com>
2017-07-23 19:18:40 -0700

    fix bug

--- mlat/client/coordinator.py
+++ mlat/client/coordinator.py
@@ -187,11 +187,11 @@ class Coordinator:
                 rate = 1.0 * ac.recent_adsb_positions / interval
                 ac.rate_measurement_start = now
                 ac.recent_adsb_positions = 0
-                if ac.address <0xFF0000:
+                if ac.icao <0xFF0000:
                     rate_report[ac.icao] = rate
                 else:
                     if rate>10:
-                        rate_report[ac.icao] = rates
+                        rate_report[ac.icao] = rate
 
         if rate_report:
             self.server.send_rate_report(rate_report)
